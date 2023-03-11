ec0869df9814b420065c23fc029b312f461988b0
sambath127 <65406467+sambath127@users.noreply.github.com>
2023-03-11 12:46:08 +0530

    Add files via upload
    Week2 Assignments

---
+++ Day2/MissingElementInAnArray.java
@@ -0,0 +1,18 @@
+package week1.Day2;
+
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,7,6,8};
+	Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+		if(arr[i]!=i+1) {
+			System.out.println(i+1);
+			break;
+			
+		}
+	}}
+	
+}
